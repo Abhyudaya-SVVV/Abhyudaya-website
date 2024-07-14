dee1836e077796cdfb82282f8963ae51981441b8
Pranjal birla <birlapranjal460@gmail.com>
2024-07-14 23:43:22 +0530

    added responsiveness to the faculty section

--- src/components/FacultySection.tsx
+++ src/components/FacultySection.tsx
@@ -15,21 +15,15 @@ type FacultySectionProps = {
 
 const FacultySection = ({ faculties }: FacultySectionProps) => {
   const topManagement = faculties[0].post === "Vice Chancellor";
-  const cardsClass = cx({
-    flex: true,
-    "flex-wrap": true,
-    "gap-16": true,
-    "justify-center": true,
-    "gap-x-32": topManagement,
-  });
+
   return (
-    <section className="w-11/12 mx-auto flex flex-col items-center gap-x-16 gap-y-4 ">
+    <section className="w-11/12 mx-auto flex flex-col items-center  gap-y-4 ">
       <h1
         className={`text-3xl text-center lg:text-start lg:text-4xl  font-bold ${turret.className}`}
       >
         {topManagement ? "VC & HOD" : "Faculty coordinator and mentor"}
       </h1>
-      <div className={cardsClass}>
+      <div className= {` flex w-11/12  justify-center gap-5 md:gap-16 flex-wrap `}>
         {faculties?.map((person) => (
           <FacultyCard
             key={person.id}
